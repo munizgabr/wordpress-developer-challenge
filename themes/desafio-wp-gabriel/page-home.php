9876402ddeda873b8c16c8446612aaceac76a432
munizgabr <gabrielrrtolkien@gmail.com>
2022-11-09 07:06:02 -0300

    Finish slider

--- themes/desafio-wp-gabriel/page-home.php
+++ themes/desafio-wp-gabriel/page-home.php
@@ -10,19 +10,178 @@ get_header();
         <div class="video__content">Lorem ipsum dolor</div>
         <div class="video__btn"><a href="#">Mais informações</a></div>
     </div>
-    <div class="swiper mySwiper">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">Slide 1</div>
-            <div class="swiper-slide">Slide 2</div>
-            <div class="swiper-slide">Slide 3</div>
-            <div class="swiper-slide">Slide 4</div>
-            <div class="swiper-slide">Slide 5</div>
-            <div class="swiper-slide">Slide 6</div>
-            <div class="swiper-slide">Slide 7</div>
-            <div class="swiper-slide">Slide 8</div>
-            <div class="swiper-slide">Slide 9</div>
+    <div class="swiper-content">
+        <span class="swiper-title center">Filmes</span>
+        <div class="swiper filme">
+            <div class="swiper-wrapper swiper-position">
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+            </div>
         </div>
-        <div class="swiper-pagination"></div>
+    </div>
+
+    <div class="swiper-content">
+        <span class="swiper-title center">Documentários</span>
+        <div class="swiper documentario">
+            <div class="swiper-wrapper swiper-position">
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="swiper-content">
+        <span class="swiper-title center">Séries</span>
+        <div class="swiper serie">
+            <div class="swiper-wrapper swiper-position">
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+                <div class="swiper-slide swiper-modify">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
+                    <span class="video__cat--duration">130M</span>
+                    <div class="video__content">Lorem ipsum dolor</div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- Swiper JS -->
@@ -30,14 +189,22 @@ get_header();
 
     <!-- Initialize Swiper -->
     <script>
-        var swiper = new Swiper(".mySwiper", {
+        var swiper = new Swiper(".filme", {
             slidesPerView: 6,
-            spaceBetween: 20,
+            spaceBetween: 25,
             centeredSlides: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
+        });
+
+        var swiper = new Swiper(".documentario", {
+            slidesPerView: 6,
+            spaceBetween: 25,
+            centeredSlides: true,
+        });
+
+        var swiper = new Swiper(".serie", {
+            slidesPerView: 6,
+            spaceBetween: 25,
+            centeredSlides: true,
         });
     </script>
     <?php
