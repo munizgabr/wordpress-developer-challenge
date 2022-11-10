7b5e97fca0ffdba4ff3af46fbb837a79d905374f
munizgabr <gabrielrrtolkien@gmail.com>
2022-11-09 21:41:35 -0300

    Add a custom thumb size

--- themes/desafio-wp-gabriel/functions.php
+++ themes/desafio-wp-gabriel/functions.php
@@ -1,29 +1,24 @@
 <?php
 
-define( 'somadev_VERSION', '0.0.1' );
 add_theme_support( 'post-thumbnails' );
 
 if (function_exists('add_image_size')) {
-  //add_image_size( 'thumb_slider_desktop', 1920, 880, true );
+  add_image_size( 'thumb_1', 225, 340, true );
 }
+// Custom post
+require(get_template_directory() . '/functions/custompost/video.php' );
 
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
-
-// Functions
-require(get_template_directory() . '/functions/scripts-footer.php');
-require(get_template_directory() . '/functions/login-style.php' );
-
-// Widgets
-require(get_template_directory() . '/functions/widgets.php' );
-
-// Custom Post
-
-// ACF
-require(get_template_directory() . '/functions/acf/scripts-header.php' );
-require(get_template_directory() . '/functions/acf/scripts-footer.php' );
-require(get_template_directory() . '/functions/acf/config-page.php' );
-
-function isMobile() {
-    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+function example_insert_category() {
+  $parent_term = term_exists( 'video' ); // array is returned if taxonomy is given
+$parent_term_id = $parent_term['term_id']; // get numeric term id
+  wp_insert_term(
+      'Filme',
+      'Série',
+      'Documentário',
+      array(
+        'description' => 'This is an example category created with wp_insert_term.',
+        'slug'        => 'tipo-video'
+      )
+  );
 }
+add_action( 'after_setup_theme', 'example_insert_category' );
