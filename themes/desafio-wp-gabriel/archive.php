973145a28317af49e002fe506e0ea0b4278c4487
munizgabr <gabrielrrtolkien@gmail.com>
2022-11-11 16:00:55 -0300

    Fix archive

--- themes/desafio-wp-gabriel/archive.php
+++ themes/desafio-wp-gabriel/archive.php
@@ -12,13 +12,12 @@ $term = array_shift( $terms );
     <div class="video__archive">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
         $thumbnail = get_the_post_thumbnail_url(get_the_ID(),'thumb_1');?>
-        <div class="swiper-slide swiper-modify">
+        <div class="swiper-modify">
             <?php if(!empty($thumbnail)){
                 echo '<img src="'.$thumbnail.'" alt="'.get_the_title().'">';
             } else {
                 echo '<img src="'.$blogInfo.'/assets/img/pexels-gabb-tapic-3568544.jpg" alt="'.get_the_title().'">';
             } ?>
-            <!-- <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título"> -->
             <span class="video__cat--duration"><?php echo $duration;?>M</span>
             <div class="video__content"><a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php the_title();?></a></div>
         </div>
