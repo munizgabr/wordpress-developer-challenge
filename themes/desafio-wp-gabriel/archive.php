bb1e6dc3824befda37315c07a4789cbe9bc8529b
munizgabr <gabrielrrtolkien@gmail.com>
2022-11-11 19:37:21 -0300

    Add tag a to video archive

--- themes/desafio-wp-gabriel/archive.php
+++ themes/desafio-wp-gabriel/archive.php
@@ -12,11 +12,11 @@ $term = array_shift( $terms );
     <div class="video__archive">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
         $thumbnail = get_the_post_thumbnail_url(get_the_ID(),'thumb_1');?>
-        <div class="swiper-modify">
+        <div class="swiper-slide swiper-modify">
             <?php if(!empty($thumbnail)){
-                echo '<img src="'.$thumbnail.'" alt="'.get_the_title().'">';
+                echo '<a href="'.get_the_permalink().'" title="'.get_the_title().'"><img src="'.$thumbnail.'" alt="'.get_the_title().'"></a>';
             } else {
-                echo '<img src="'.$blogInfo.'/assets/img/pexels-gabb-tapic-3568544.jpg" alt="'.get_the_title().'">';
+                echo '<a href="'.$link.'" title="'.$title.'"><img src="'.$blogInfo.'/assets/img/pexels-gabb-tapic-3568544.jpg" alt="'.get_the_title().'"></a>';
             } ?>
             <span class="video__cat--duration"><?php echo $duration;?>M</span>
             <div class="video__content"><a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php the_title();?></a></div>
