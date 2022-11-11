628364e0f50034c025c34bc527a1c16f35bce24e
munizgabr <gabrielrrtolkien@gmail.com>
2022-11-10 21:17:14 -0300

    archive backend

--- themes/desafio-wp-gabriel/archive.php
+++ themes/desafio-wp-gabriel/archive.php
@@ -1,26 +1,25 @@
 <?php
 get_header();
+$thumbnail = get_the_post_thumbnail_url(get_the_ID(),'thumb_1');
+$duration = get_field('duration');
+$terms  = get_the_terms( $post->ID, 'tipo' );
+$term = array_shift( $terms );
 ?>
 <div class="center wrapper-archive">
     <div class="descriptiom">
-        <span class="video__cat--cat">Filmes</span>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eveniet beatae amet ipsa eos sunt tempore, quam reprehenderit sint qui repellat a nisi debitis eligendi dolorum quia, accusamus expedita mollitia.</p>
+        <span class="title"><?php echo $term->name?></span>
+        <p><?php echo $term->description;?></p>
     </div>
-    <div class="videos">
+    <div class="video__archive">
         <div class="swiper-slide swiper-modify">
-            <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
-            <span class="video__cat--duration">130M</span>
-            <div class="video__content"><a href="#" title="titulo">titulo</a></div>
-        </div>
-        <div class="swiper-slide swiper-modify">
-            <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
-            <span class="video__cat--duration">130M</span>
-            <div class="video__content"><a href="#" title="titulo">titulo</a></div>
-        </div>
-        <div class="swiper-slide swiper-modify">
-            <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título">
-            <span class="video__cat--duration">130M</span>
-            <div class="video__content"><a href="#" title="titulo">titulo</a></div>
+            <?php if(!empty($thumbnail)){
+                echo '<img src="'.$thumbnail.'" alt="'.get_the_title().'">';
+            } else {
+                echo '<img src="'.$blogInfo.'/assets/img/pexels-gabb-tapic-3568544.jpg" alt="'.get_the_title().'">';
+            } ?>
+            <!-- <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título"> -->
+            <span class="video__cat--duration"><?php echo $duration;?>M</span>
+            <div class="video__content"><a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php the_title();?></a></div>
         </div>
     </div>
 </div>
