d5b42dd7197bc2fc5a84cacc2357ac4bbd6a3555
munizgabr <gabrielrrtolkien@gmail.com>
2022-11-11 07:19:33 -0300

    Fix query

--- themes/desafio-wp-gabriel/page-home.php
+++ themes/desafio-wp-gabriel/page-home.php
@@ -1,75 +1,80 @@
 <?php
-get_header();
-$category = array(
-    'taxonomy' => 'tipo',
-    'orderby' => 'name',
-);
-$cats = get_categories($category);
+get_header(); ?>
 
-$postsPerPage = 10;
-$highlightPostsCount = 1;
-$regularPostsCount = $postsPerPage - $highlightPostsCount;
+<!-- START NEW TEST -->
 
-$args = [
-    'posts_per_page' => $postsPerPage,
-    'post_type' => 'video',
-    'tax_query' => array(
-        array(
-        'taxonomy' => 'tipo',
-        'field'    => 'name',
-        'terms'    => array(
-            'filme',
-            'serie',
-            'documentario'
-        ),
-        )
-    )
-];
-$blogInfo = get_bloginfo('template_url');
-$video = new WP_Query( $args );
-$highlights = array_slice($video->posts, 0, $highlightPostsCount);
-$regularPosts = array_slice($video->posts, 1, $regularPostsCount);
-if($video->have_posts()) :
-    ?>
+<style type="text/css">
+    .new-query, .new-query a {
+        color: #fff;
+    }
+</style>
+
+<?php
+$terms = get_terms('tipo');
+foreach( $terms as $term ):
+?>
+    <div class="new-query">                         
+        <h3><?php echo $term->name;?></h3>                          
+        <ul>
+            <?php $posts = get_posts(array(
+                'post_type' => 'video',
+                'taxonomy' => $term->taxonomy,
+                'orderby' => 'name',
+                'term' => $term->slug
+            ));
+            foreach($posts as $post):
+                setup_postdata($post);
+                $duration = get_field('duration');
+            ?> 
+            <li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?> - <span>Duração: <?php echo $duration; ?></span></a></li>    
+            <?php endforeach; ?>
+        </ul>                                                   
+    </div>
+<?php endforeach; ?>
+
+<!-- END NEW TEST -->
+
 <div class="video" style="background-image: url('<?php echo get_bloginfo('template_url'); ?>/assets/img/tower.jpg'); height: 75vh;">
     <div class="video__wrapper center">
         <div class="video__cat">
-            <span class="video__cat--cat">Filmes</span>
+            <span class="video__cat--cat"><?php echo $term->name;?></span>
             <span class="video__cat--duration">130M</span>
         </div>
         <div class="video__content"><a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php the_title();?></a></div>
         <div class="video__btn"><a href="#">Mais informações</a></div>
     </div>
+    <?php $terms = get_terms('tipo');?>
 </div>
-    <?php
-    foreach($cats as $cat) {
-    while ( $video->have_posts() ) :
-        $video->the_post();
-        $thumbnail = get_the_post_thumbnail_url(get_the_ID(),'thumb_1');
-        $duration = get_field('duration');
-        var_dump($duration);?>
+<?php foreach( $terms as $term ):?>
     <div class="swiper-content">
-        <span class="swiper-title"><?php echo $cat->name?></span>
+        <span class="swiper-title"><?php echo $term->name;?></span>
         <div class="swiper filme">
             <div class="swiper-wrapper swiper-position">
+                
+                <?php
+                $posts = get_posts(array(
+                    'post_type' => 'video',
+                    'taxonomy' => $term->taxonomy,
+                    'term' => $term->slug
+                )); 
+                foreach ($posts as $post ):
+                setup_postdata($post);
+                $duration = get_field('duration');
+                $thumbnail = get_the_post_thumbnail_url(get_the_ID(),'thumb_1');?>
                 <div class="swiper-slide swiper-modify">
                     <?php if(!empty($thumbnail)){
                         echo '<img src="'.$thumbnail.'" alt="'.get_the_title().'">';
                     } else {
                         echo '<img src="'.$blogInfo.'/assets/img/pexels-gabb-tapic-3568544.jpg" alt="'.get_the_title().'">';
                     } ?>
-                    <!-- <img src="<?php echo get_bloginfo('template_url'); ?>/assets/img/pexels-gabb-tapic-3568544.jpg" alt="Título"> -->
                     <span class="video__cat--duration"><?php echo $duration;?>M</span>
                     <div class="video__content"><a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php the_title();?></a></div>
                 </div>
-               
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
-    <?php
-    endwhile;
-        }
-    endif; ?>
+<?php endforeach; ?>
 
     <!-- Swiper JS -->
     <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
