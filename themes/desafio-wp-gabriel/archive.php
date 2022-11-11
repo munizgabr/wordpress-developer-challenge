2593316eebb0abc542c8fb2a054e7baf3864d360
munizgabr <gabrielrrtolkien@gmail.com>
2022-11-11 10:25:31 -0300

    Finish archive backend

--- themes/desafio-wp-gabriel/archive.php
+++ themes/desafio-wp-gabriel/archive.php
@@ -1,6 +1,5 @@
 <?php
 get_header();
-$thumbnail = get_the_post_thumbnail_url(get_the_ID(),'thumb_1');
 $duration = get_field('duration');
 $terms  = get_the_terms( $post->ID, 'tipo' );
 $term = array_shift( $terms );
@@ -11,6 +10,8 @@ $term = array_shift( $terms );
         <p><?php echo $term->description;?></p>
     </div>
     <div class="video__archive">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+        $thumbnail = get_the_post_thumbnail_url(get_the_ID(),'thumb_1');?>
         <div class="swiper-slide swiper-modify">
             <?php if(!empty($thumbnail)){
                 echo '<img src="'.$thumbnail.'" alt="'.get_the_title().'">';
@@ -21,6 +22,10 @@ $term = array_shift( $terms );
             <span class="video__cat--duration"><?php echo $duration;?>M</span>
             <div class="video__content"><a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php the_title();?></a></div>
         </div>
+        <?php
+        endwhile;
+        endif;
+        ?>
     </div>
 </div>
 <?php
