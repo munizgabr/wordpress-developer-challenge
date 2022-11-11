c676deb2efd039bddcabae057af967b88fc5abec
munizgabr <gabrielrrtolkien@gmail.com>
2022-11-11 19:37:11 -0300

    Adjust query highlight to show order desc

--- themes/desafio-wp-gabriel/page-home.php
+++ themes/desafio-wp-gabriel/page-home.php
@@ -1,15 +1,6 @@
 <?php
 get_header(); ?>
 
-<!-- START NEW TEST -->
-
-<style type="text/css">
-    .new-query,
-    .new-query a {
-        color: #fff;
-    }
-</style>
-
 <?php
 $terms = get_terms('tipo');
 foreach ($terms as $term) :
@@ -18,7 +9,8 @@ foreach ($terms as $term) :
         'post_type' => 'video',
         'taxonomy' => $term->taxonomy,
         'orderby' => 'name',
-        'term' => $term->slug
+        'term' => $term->slug,
+        'order' => 'DESC'
     ));
     foreach ($posts as $post) :
         setup_postdata($post);
@@ -26,9 +18,6 @@ foreach ($terms as $term) :
     ?>
     <?php endforeach; ?>
 <?php endforeach; ?>
-
-<!-- END NEW TEST -->
-
 <div class="video" style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID()) ?>'); height: 75vh;">
     <div class="video__wrapper center">
         <div class="video__cat">
@@ -56,10 +45,10 @@ foreach ($terms as $term) :
                     $duration = get_field('duration');
                     $thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'thumb_1'); ?>
                     <div class="swiper-slide swiper-modify">
-                        <?php if (!empty($thumbnail)) {
-                            echo '<img src="' . $thumbnail . '" alt="' . get_the_title() . '">';
+                        <?php if(!empty($thumbnail)){
+                            echo '<a href="'.get_the_permalink().'" title="'.get_the_title().'"><img src="'.$thumbnail.'" alt="'.get_the_title().'"></a>';
                         } else {
-                            echo '<img src="' . $blogInfo . '/assets/img/pexels-gabb-tapic-3568544.jpg" alt="' . get_the_title() . '">';
+                            echo '<a href="'.$link.'" title="'.$title.'"><img src="'.$blogInfo.'/assets/img/pexels-gabb-tapic-3568544.jpg" alt="'.get_the_title().'"></a>';
                         } ?>
                         <span class="video__cat--duration"><?php echo $duration; ?>M</span>
                         <div class="video__content"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></div>
